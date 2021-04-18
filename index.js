3a8215818f7fbac7dce86046f56b0fe382629bb5
hj23asd@outlook.com (Ilbko) <hj23asd@outlook.com>
2021-04-18 15:13:49 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+$('#but').click(function(){
+    var i = $("int").value();
+    alert("He");
+    for (let i = 0; i < array.length; i++) {
+        $("body").append('<br/><input type="text" id="inp">');
+    }
+});
+
+   
